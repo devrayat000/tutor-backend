e31e34f683576679628cb2c519fb9e0cde519abc
Zul Ikram Musaddik Rayat <2010050@me.buet.ac.bd>
2022-11-17 01:55:21 +0600

    added static url in server.js

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "127.0.0.1"),
   port: env.int("PORT", 1337),
+  url: `https://${env("RAILWAY_STATIC_URL", "mydomain.wtf")}`,
   app: {
     keys: env.array("APP_KEYS"),
   },
