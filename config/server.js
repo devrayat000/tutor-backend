db7b8ab150cbc36afd0f9ef9a571f2a733819325
Zul Ikram Musaddik Rayat <2010050@me.buet.ac.bd>
2022-11-17 01:56:13 +0600

    changed back to 0.0.0.0

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "127.0.0.1"),
+  host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   url: `https://${env("RAILWAY_STATIC_URL", "mydomain.wtf")}`,
   app: {
