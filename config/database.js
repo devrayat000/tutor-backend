e4cbc9f02b244e375e379e926e8f896fb4940264
Zul Ikram Musaddik Rayat <2010050@me.buet.ac.bd>
2022-11-16 23:57:53 +0600

    finalized

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
+      host: env("PGHOST", "127.0.0.1"),
+      port: env.int("PGPORT", 5432),
+      database: env("PGDATABASE", "tutorbd"),
+      user: env("PGUSER", "postgres"),
+      password: env("PGPASSWORD", "ppooii12"),
       ssl: env.bool(true),
     },
   },
